39f61517fb998e824bfa80d1bb1400227e23a921
Lora <cruzeiro@abv.bg>
2021-01-02 17:48:42 +0200

    Initial commit

---
+++ src/Simple_Calculations/SquareArea.java
@@ -0,0 +1,12 @@
+package Simple_Calculations;
+
+import java.util.Scanner;
+
+public class SquareArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(a * a);
+    }
+}
